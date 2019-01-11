0511a8941f3d527016acd78e701ce67ef906118c
Arveen <arveen@feed.fm>
2019-01-11 11:42:22 -0800

    Update to latest version of sdk

--- radioplayer/src/main/java/fm/feed/android/radioplayer/Application.java
+++ radioplayer/src/main/java/fm/feed/android/radioplayer/Application.java
@@ -5,14 +5,11 @@ package fm.feed.android.radioplayer;
  */
 
 import fm.feed.android.playersdk.FeedPlayerService;
-import com.crashlytics.android.Crashlytics;
-import io.fabric.sdk.android.Fabric;
 
 public class Application extends android.app.Application {
 
     public void onCreate() {
         super.onCreate();
-        Fabric.with(this, new Crashlytics());
 
         // initialize player
         FeedPlayerService.initialize(getApplicationContext(),"demo", "demo");
